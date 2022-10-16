1d9dbe885558a4474118abbfc5bbebd4f7b2d3d7
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-10-16 18:44:21 +0700

    ⬆️⬆️⬆️

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
+import 'package:simantan/app/modules/auth/bindings/auth_binding.dart';
 
+import '../modules/auth/views/auth_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 
@@ -11,6 +13,11 @@ class AppPages {
   static const INITIAL = Routes.HOME;
 
   static final routes = [
+    GetPage(
+      name: _Paths.AUTH,
+      page: () => AuthView(),
+      binding: AuthBinding(),
+    ),
     GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
