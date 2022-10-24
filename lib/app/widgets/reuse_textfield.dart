c6ec48d522c51e9990235b620c937992acdbc707
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-10-24 22:57:13 +0700

    ⬆️⬆️⬆️Create Reuse button

--- lib/app/widgets/reuse_textfield.dart
+++ lib/app/widgets/reuse_textfield.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:simantan/app/theme/colors.dart';
 
 class ReuseTextField extends StatelessWidget {
   final TextEditingController? controller;
   final String? hintText;
   final String? labelText;
+  final IconData? prefixIcon;
+  final IconData? suffixIcon;
   final int? maxLines;
   final int? minLines;
   final bool? obscureText;
+
   final IconData? icon;
   final TextInputType? keyboardType;
   final Function? onChanged;
   final Function? onTap;
   final Function? onSubmitted;
 
-  const ReuseTextField({
+  // final Function? onEditingComplete;
+  final dynamic validator;
+  final dynamic onPressSuffix;
+
+  ReuseTextField({
     super.key,
     this.controller,
     this.hintText,
@@ -26,6 +34,10 @@ class ReuseTextField extends StatelessWidget {
     this.labelText,
     this.maxLines,
     this.minLines,
+    this.validator,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.onPressSuffix,
   });
 
   @override
@@ -33,12 +45,26 @@ class ReuseTextField extends StatelessWidget {
     return TextFormField(
       minLines: minLines,
       maxLines: maxLines,
+      validator: validator,
+      obscureText: obscureText ?? false,
       decoration: InputDecoration(
+        suffixIcon: IconButton(
+          onPressed: onPressSuffix,
+          icon: Icon(
+            suffixIcon,
+            color: SchemaColor.primary,
+          ),
+        ),
+        prefixIcon: Icon(
+          prefixIcon,
+          color: SchemaColor.primary,
+        ),
         // labelText: "Deskripsi Di Sini Yaa",
         hintText: hintText,
+        labelText: labelText,
         labelStyle: TextStyle(
             // color: Color.fromARGB(255, 137, 126, 255),
-            ),
+            color: Colors.grey),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
           borderSide: BorderSide(
@@ -47,7 +73,7 @@ class ReuseTextField extends StatelessWidget {
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(
+          borderSide: BorderSide(color: SchemaColor.primary
               // color: Color.fromARGB(255, 137, 126, 255),
               ),
         ),
