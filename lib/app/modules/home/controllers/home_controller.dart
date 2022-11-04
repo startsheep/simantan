e0f083dedc567fe5e2c94f370a94af9acff3b199
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-11-04 22:49:57 +0700

    ⬆️⬆️⬆️Update Binding

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,21 +1,27 @@
 import 'package:get/get.dart';
+import 'package:simantan/app/controllers/comment_controller.dart';
 import 'package:simantan/app/controllers/post_controller.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
 
   late PostController postController;
+  late CommentController commentController;
   @override
   void onInit() {
     super.onInit();
     Get.lazyPut<PostController>(() => PostController());
+    Get.lazyPut<CommentController>(() => CommentController());
+    commentController = Get.find<CommentController>();
     postController = Get.find<PostController>();
+    commentController.onInit();
     postController.onInit();
     postController.fetchPosts();
   }
 
   @override
   void onReady() {
+    Get.lazyPut<CommentController>(() => CommentController());
     Get.lazyPut<PostController>(() => PostController());
     super.onReady();
     postController.fetchPosts();
