c1f5589d79db8cecbe30398fbebeecae24e6d403
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-11-04 22:48:51 +0700

    🔨🔨🔨Update Post View  Add Section Comment

--- lib/app/modules/post/views/post_view.dart
+++ lib/app/modules/post/views/post_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:simantan/app/controllers/post_controller.dart';
 import 'package:simantan/app/modules/post/views/widget_upload..dart';
+import 'package:simantan/app/theme/colors.dart';
 import 'package:simantan/app/widgets/button_primary.dart';
 import 'package:simantan/app/widgets/reuse_dropdown_search.dart';
 import 'package:simantan/app/widgets/reuse_textfield.dart';
@@ -16,68 +17,71 @@ class PostView extends GetView<PostController> {
           child: Padding(
             padding: const EdgeInsets.all(15),
             child: Obx(() {
-              return Column(
-                children: [
-                  const WidgetUpload(),
-                  const Divider(),
-                  Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        ReuseTextField(
-                          controller: controller.descriptionController.value,
-                          validator: (val) {
-                            if (val == null || val.isEmpty) {
-                              return "deskripsi tidak boleh kosong";
-                            }
-                            return null;
-                          },
-                          labelText: "Deskripsi",
-                          hintText: "Deskrpsi",
-                          icon: Icons.title,
-                          minLines: 1,
-                          maxLines: 10,
-                        ),
-                        // Drop
-                        const Divider(),
-
-                        // Button Unggah
-                        const Divider(),
-                        ReuseDropDownSearch(
-                          title: "Tagar",
-                          asyncItems: controller
-                              .fetchFlags(controller.searchFlag.value.text),
-                          onChange: (val) {
-                            controller.flagId.value = val["id"];
-                          },
-                          itemAsString: (item) {
-                            return item['name'].toString();
-                          },
-                          filterFn: (item, text) {
-                            return item['name']
-                                .toString()
-                                .toLowerCase()
-                                .contains(text.toLowerCase());
-                          },
-                        ),
-                        // create dropdown manualy with search
-
-                        const Divider(),
-                        ButtonPrimary(
-                          label: "Unggah",
-                          onPressed: () {
-                            if (_formKey.currentState!.validate() &&
-                                controller.flagId.value != null &&
-                                controller.image.value != null &&
-                                controller.image.value.path.isNotEmpty) {
-                              controller.storePost();
-                            }
-                          },
-                        ),
-                      ],
+              return Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const WidgetUpload(),
+                    const Divider(),
+                    ReuseTextField(
+                      controller: controller.descriptionController.value,
+                      validator: (val) {
+                        if (val == null || val.isEmpty) {
+                          return "deskripsi tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                      labelText: "Deskripsi",
+                      hintText: "Deskrpsi",
+                      icon: Icons.title,
+                      minLines: 1,
+                      maxLines: 10,
                     ),
-                  )
-                ],
+                    // Drop
+
+                    // Button Unggah
+                    const Divider(),
+                    ReuseDropDownSearch(
+                      title: "Tagar",
+                      asyncItems: controller
+                          .fetchFlags(controller.searchFlag.value.text),
+                      onChange: (val) {
+                        controller.flagId.value = val["id"];
+                      },
+                      itemAsString: (item) {
+                        return item['name'].toString();
+                      },
+                      filterFn: (item, text) {
+                        return item['name']
+                            .toString()
+                            .toLowerCase()
+                            .contains(text.toLowerCase());
+                      },
+                    ),
+                    // create dropdown manualy with search
+
+                    const Divider(),
+                    controller.isUploading.value
+                        ? const Center(
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                SchemaColor.primary,
+                              ),
+                            ),
+                          )
+                        : ButtonPrimary(
+                            label: "Unggah",
+                            onPressed: () {
+                              if (_formKey.currentState!.validate() &&
+                                  controller.flagId.value != null &&
+                                  controller.image.value != null &&
+                                  controller.image.value.path.isNotEmpty) {
+                                controller.storePost();
+                              }
+                            },
+                          ),
+                  ],
+                ),
               );
             }),
           ),
