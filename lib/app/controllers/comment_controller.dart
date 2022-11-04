8cbfe05b84bce62fabfa1a50ea8d89c12c59dbd3
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-11-04 22:47:13 +0700

    ⬆️⬆️⬆️Create Comment Controler

---
+++ lib/app/controllers/comment_controller.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:simantan/app/models/lazy_loading_filter.dart';
+import 'package:simantan/app/providers/comment_provider.dart';
+
+class CommentController extends GetxController {
+  final Rx<TextEditingController> message = TextEditingController().obs;
+  final List<TextEditingController> messagesController =
+      <TextEditingController>[].obs;
+  final RxList comments = [].obs;
+  final RxBool isLoading = true.obs;
+
+  final _paginationFilter = LazyLoadingFilter().obs;
+  final _lastPage = false.obs;
+  int get _limit => _paginationFilter.value.limit!;
+  int get _page => _paginationFilter.value.page!;
+  bool get lastPage => _lastPage.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    Get.lazyPut<CommentProvider>(() => CommentProvider());
+    Get.find<CommentProvider>().onInit();
+    print("Comment Controller init");
+    ever(_paginationFilter, (_) => getComments());
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+
+  void getComments() async {
+    print("getComments");
+
+    final response =
+        await Get.find<CommentProvider>().getComments(_paginationFilter.value);
+
+    if (response.statusCode == 200) {
+      isLoading.value = false;
+      final _comments = json.encode(response.body['data']);
+      comments.value = json.decode(_comments);
+    }
+  }
+
+  void storeComment({
+    String? postId,
+    String? message,
+  }) async {
+    final response = await Get.find<CommentProvider>().storeComment(
+      message: message,
+      postId: postId,
+    );
+    print(response.statusCode);
+    if (response.status.hasError) {
+      Get.snackbar('Error', response.statusText.toString());
+    } else {
+      Get.snackbar('Success', response.statusText.toString());
+    }
+  }
+
+  void changePaginationFilter(int page, int limit) {
+    _paginationFilter.update((val) {
+      val!.page = page;
+      val.limit = limit;
+    });
+  }
+
+  void changeTotalPerPage(int limitval) {
+    comments.clear();
+    _lastPage.value = false;
+    changePaginationFilter(1, limitval);
+  }
+
+  void loadNextPage() => changePaginationFilter(_page + 1, _limit);
+}
