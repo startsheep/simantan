8a91fd092e61298d33c7970b4b166d721bf0e745
Nandang Eka Prasetya <ekaprasetya2244@gmail.com>
2022-12-04 19:13:23 +0700

    ⬆️⬆️⬆️

--- lib/app/modules/home/widgets/post_actions.dart
+++ lib/app/modules/home/widgets/post_actions.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:images_picker/images_picker.dart';
 import 'package:like_button/like_button.dart';
 import 'package:simantan/app/controllers/post_controller.dart';
 import 'package:simantan/app/modules/post/controllers/downloader_controller.dart';
@@ -11,16 +14,20 @@ class PostActions extends GetView<LikeController> {
   int? postId;
   int? likeCount;
   String? pathImage;
+  String? userName;
   PostActions({
     this.postId,
     this.likeCount,
     this.pathImage,
+    this.userName,
     Key? key,
   }) : super(key: key);
   // final controller = Get.find<PostController>();
 
   @override
   Widget build(BuildContext context) {
+    Get.lazyPut<DownloaderController>(() => DownloaderController());
+    // final downloadController = Get.;
     // var value;
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -86,15 +93,25 @@ class PostActions extends GetView<LikeController> {
                     parameters: {'postId': postId.toString()!});
               },
             ),
-            IconButton(
-              icon: const Icon(Icons.download_outlined),
-              onPressed: () async {
-                Get.lazyPut<DownloaderController>(() => DownloaderController());
-                await Get.find<DownloaderController>()
-                    .downloadImage(pathImage.toString()!, whenError: true);
-                Get.find<DownloaderController>().onInit();
-              },
-            ),
+            Obx(() {
+              return Get.find<DownloaderController>().downloading.value
+                  ? CircularProgressIndicator(
+                      // key: valu,
+                      value: Get.find<DownloaderController>().progress.value,
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        SchemaColor.primary,
+                      ),
+                      strokeWidth: 3,
+                    )
+                  : IconButton(
+                      icon: const Icon(Icons.download_outlined),
+                      onPressed: () {
+                        Get.find<DownloaderController>().doDownload(
+                            pathImage, (userName! + postId.toString()));
+                      },
+                    );
+            }),
+
             // Text(controller..value),
           ],
         ),
@@ -116,6 +133,31 @@ class PostActions extends GetView<LikeController> {
         } else {
           return const Icon(Icons.favorite_border);
         }
+      },
+    );
+  }
+
+  void save(String url) async {
+    // File file = await Get.find<DownloaderController>().downloadFIle(url);
+    // bool res = await ImagesPicker.saveImageToAlbum(file, albumName: "");
+    // print(res);
+  }
+  void showDialogLoading() {
+    // get loafin dialog and value from download controller
+    String progress = Get.find<DownloaderController>().progressString.value;
+    bool isLoading = Get.find<DownloaderController>().downloading.value;
+    // show dialog
+    Get.defaultDialog(
+      title: "Downloading",
+      content: Column(
+        children: [
+          CircularProgressIndicator(),
+          Text(progress),
+        ],
+      ),
+      barrierDismissible: false,
+      onWillPop: () async {
+        return false;
       },
     );
   }
--- lib/app/modules/home/widgets/post_content.dart
+++ lib/app/modules/home/widgets/post_content.dart
@@ -25,32 +25,39 @@ class PostContent extends StatelessWidget {
           ),
         ],
       ),
-      child: Image.network(
-        contentUrl!,
-        loadingBuilder: (context, child, loadingProgress) {
-          if (loadingProgress == null) return child;
-          return Container(
-            // place holder image from assets
-            width: Get.width,
-            height: Get.width,
-            child: Center(
-              child: CircularProgressIndicator(
-                value: loadingProgress.expectedTotalBytes != null
-                    ? loadingProgress.cumulativeBytesLoaded /
-                        loadingProgress.expectedTotalBytes!
-                    : null,
-              ),
-            ),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/placeholder_image.jpg'),
-                fit: BoxFit.cover,
-              ),
-              color: Colors.grey[200],
-              borderRadius: BorderRadius.circular(10),
-            ),
-          );
+      child: InteractiveViewer(
+        onInteractionEnd: (details) {
+          //set the scale to 1.0
         },
+        // boundaryMargin: EdgeInsets.all(20.0),
+        alignPanAxis: true,
+        child: Image.network(
+          contentUrl!,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Container(
+              // place holder image from assets
+              width: Get.width,
+              height: Get.width,
+              child: Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes!
+                      : null,
+                ),
+              ),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/placeholder_image.jpg'),
+                  fit: BoxFit.cover,
+                ),
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(10),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
